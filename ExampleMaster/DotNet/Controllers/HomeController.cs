0859ec486add3d65a84ea5849e6e89d3dd95512c
Harshit Gindra <harshitgindra@gmail.com>
2019-01-11 06:56:44 +0000

    Initial Commit

---
+++ ExampleMaster/DotNet/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using DotNet.Core;
+using System.Diagnostics;
+using System.Threading;
+using System.Web.Mvc;
+
+namespace DotNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public CancellationTokenSource TokenSource;
+
+        public HomeController()
+        {
+
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void StartTask()
+        {
+            AppConstants.TokenSource = new CancellationTokenSource();
+            for (int i = 0; i < 20; i++)
+            {
+                if (!AppConstants.TokenSource.Token.IsCancellationRequested)
+                {
+                    Debug.WriteLine($"Loop:{i}");
+                    Thread.Sleep(2000);
+                }
+                else
+                {
+                    Debug.WriteLine($"Cancellation Token Requested");
+                    break;
+                }
+            }
+        }
+
+        [HttpPost]
+        public void StopTask()
+        {
+            if (AppConstants.TokenSource != null)
+            {
+                AppConstants.TokenSource.Cancel();
+            }
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ExampleMaster/DotNet/Core/AppConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+namespace DotNet.Core
+{
+    public static class AppConstants
+    {
+        public static CancellationTokenSource TokenSource { get; set; }
+    }
+}
